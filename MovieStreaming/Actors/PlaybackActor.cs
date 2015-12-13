a2ba563f43e1875d67038239c1c6cdb8a7042eda
Ody Mbegbu <odytrice@gmail.com>
2015-12-13 15:06:34 +0100

    Logging Todos Implemented

--- MovieStreaming/Actors/PlaybackActor.cs
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -1,41 +1,41 @@
 ﻿using System;
 using Akka.Actor;
-
+using Akka.Event;
 
 namespace MovieStreaming.Actors
 {
     public class PlaybackActor : ReceiveActor
-    {     
+    {
+        private ILoggingAdapter _logger = Context.GetLogger();
+
         public PlaybackActor()
         {           
             Context.ActorOf(Props.Create<UserCoordinatorActor>(), "UserCoordinator");
             Context.ActorOf(Props.Create<PlaybackStatisticsActor>(), "PlaybackStatistics");        
         }
-
-
-
+        
 
         #region Lifecycle hooks
         protected override void PreStart()
         {
-            // TODO: log: PlaybackActor PreStart
+            _logger.Debug("PlaybackActor PreStart");
         }
 
         protected override void PostStop()
         {
-            // TODO: log: PlaybackActor PostStop
+            _logger.Debug("PlaybackActor PostStop");
         }
 
         protected override void PreRestart(Exception reason, object message)
         {
-            // TODO: log: PlaybackActor PreRestart because reason
+            _logger.Debug($"PlaybackActor PreRestart because {reason}");
 
             base.PreRestart(reason, message);
         }
 
         protected override void PostRestart(Exception reason)
         {
-            // TODO: log: PlaybackActor PostRestart because reason
+            _logger.Debug($"PlaybackActor PostRestart because {reason}");
 
             base.PostRestart(reason);
         } 
